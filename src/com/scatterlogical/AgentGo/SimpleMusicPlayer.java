a70e93a4a973f0038b64d718a035dd85debdae52
TheVoidReturns <robingpeel@gmail.com>
2015-10-15 22:54:21 +0100

    Second commit test

---
+++ src/com/scatterlogical/AgentGo/SimpleMusicPlayer.java
@@ -0,0 +1,62 @@
+package com.scatterlogical.AgentGo;
+
+public class SimpleMusicPlayer implements iMusicPlayer
+{
+	String [] allMusicToPlay;
+	String [] allPowerMusicToPlay;
+	public SimpleMusicPlayer(){
+		allMusicToPlay = new String[] {"bvtwtest.mp3"};
+		
+	}
+	@Override
+	public Boolean isRunning()
+	{
+		// TODO: Implement this method
+		return null;
+	}
+
+	@Override
+	public Boolean isPlaying()
+	{
+		// TODO: Implement this method
+		return null;
+	}
+
+	@Override
+	public void Play()
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void Pause()
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void Stop()
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void PowerStart()
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void PowerPause()
+	{
+		// TODO: Implement this method
+	}
+
+	@Override
+	public void PowerStop()
+	{
+		// TODO: Implement this method
+	}
+
+		
+}
